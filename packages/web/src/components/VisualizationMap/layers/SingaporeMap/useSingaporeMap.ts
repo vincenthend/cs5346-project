3b523f05ba7072bbe561ef222e2f883a628a5b87
Vincent H <vincenthendrha@gmail.com>
2025-05-02 22:23:08 +0800

    feat: mapbox token

--- packages/web/src/components/VisualizationMap/layers/SingaporeMap/useSingaporeMap.ts
+++ packages/web/src/components/VisualizationMap/layers/SingaporeMap/useSingaporeMap.ts
@@ -3,10 +3,11 @@ import TileLayer from 'ol/layer/Tile'
 import { fromLonLat } from 'ol/proj'
 import { XYZ } from 'ol/source'
 import View from 'ol/View'
-import env from '../../../../constants/env.ts'
 import useMap from '../../../../hooks/useMap.ts'
 
-const MAPBOX_TOKEN = env.MAPBOX_TOKEN
+declare const __MAPBOX_TOKEN: string
+
+const MAPBOX_TOKEN = __MAPBOX_TOKEN
 const MAPBOX_URL = `https://api.mapbox.com/styles/v1/vincenthendrha/cm8g5my47007101sded43brxa/tiles/512/{z}/{x}/{y}?access_token=${MAPBOX_TOKEN}`
 const SINGAPORE_COORDS = fromLonLat([103.8198, 1.3521])
 const MIN_SINGAPORE = fromLonLat([103.6, 1.2])
--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __MAPBOX_TOKEN: JSON.stringify(process.env.MAPBOX_TOKEN)
+  },
   server: {
     port: 3000
   }
